3ebc3ada546ad15d7342243ab325729bb23dd35e
Tom Ritter <tom@ritter.vg>
2020-04-06 23:37:46 -0500

    Turn Bugzilla into a BugzillaProvider

--- components/bugzilla.py
+++ components/bugzilla.py
@@ -8,24 +8,27 @@ from apis.bugzilla_api import fileBug, commentOnBug
 from components.dbmodels import JOBSTATUS
 from components.utilities import logEntryExit
 
+class DefaultBugzillaProvider:
+    def __init__(self, config):
+        pass
 
-@logEntryExit
-def file_bug(library, new_release_version):
-    summary = "Update %s to new version %s" % (
-        library.shortname, new_release_version)
-    description = ""
+    @logEntryExit
+    def file_bug(self, library, new_release_version):
+        summary = "Update %s to new version %s" % (
+            library.shortname, new_release_version)
+        description = ""
 
-    bugID = fileBug(library.bugzilla_product,
-                    library.bugzilla_component, summary, description)
-    print("Filed Bug with ID", bugID)
-    return bugID
+        bugID = fileBug(library.bugzilla_product,
+                        library.bugzilla_component, summary, description)
+        print("Filed Bug with ID", bugID)
+        return bugID
 
+    @logEntryExit
+    def comment_on_bug(self, bug_id, status, try_run=None):
+        if status == JOBSTATUS.COULD_NOT_VENDOR:
+            comment = "./mach vendor failed with the following message: <TODO>"
+        else:
+            comment = "I've submitted a try run for this commit: " + try_run
+        commentID = commentOnBug(bug_id, comment)
+        print("Filed Comment with ID %s on Bug %s" % (commentID, bug_id))
 
-@logEntryExit
-def comment_on_bug(bug_id, status, try_run=None):
-    if status == JOBSTATUS.COULD_NOT_VENDOR:
-        comment = "./mach vendor failed with the following message: <TODO>"
-    else:
-        comment = "I've submitted a try run for this commit: " + try_run
-    commentID = commentOnBug(bug_id, comment)
-    print("Filed Comment with ID %s on Bug %s" % (commentID, bug_id))

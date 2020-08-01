ebcc8c54bdcd78012e5baa1bb96f3301775265e8
Tom Ritter <tom@ritter.vg>
2020-07-31 21:20:17 -0500

    Add Treeherder APIs for retrieving and transforming job details

---
+++ tests/mock_treeherder_server.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import os
+
+from http import server
+
+FAILURE_CLASSIFICATIONS = """[{"id":7,"name":"autoclassified intermittent"},{"id":3,"name":"expected fail"},{"id":2,"name":"fixed by commit"},{"id":5,"name":"infra"},{"id":4,"name":"intermittent"},{"id":1,"name":"not classified"}]"""
+
+EXPECTEDPATH_PUSH = "push/?revision="
+EXPECTEDPATH_JOBS = "jobs/?push_id="
+EXPECTEDPATH_FAILURECLASSIFICATION = "/failureclassification"
+
+TRY_REVISIONS = {
+    'rev_broken': "{\"results\":[{\"missing_id\":0}]}",
+    'rev_good': "{\"results\":[{\"id\":1}]}",
+}
+
+PUSH_IDS = {
+    '1_1' : "treeherder_api_response_1.txt",
+    '1_2' : "treeherder_api_response_2.txt",
+}
+
+seen_counters = {}
+
+
+def find_and_increment_seen_counter(key):
+    if key not in seen_counters:
+        seen_counters[key] = 1
+    else:
+        seen_counters[key] += 1
+    return str(seen_counters[key])
+
+
+def get_appropriate_filename(path):
+    prefix = "tests/" if not os.getcwd().endswith("tests") else ""
+
+    has_page = "&page=" in path
+    i1 = path.index(EXPECTEDPATH_JOBS) + len(EXPECTEDPATH_JOBS)
+    i2 = path.index("&page=") if has_page else len(path)
+    push_id = path[i1:i2]
+
+    page = path[path.index("&page=") + len("&page="):] if has_page else "1"
+    print("Chcking for push_id", push_id, "page", page)
+
+    key = push_id + "_" + page
+
+    if key not in PUSH_IDS:
+        assert False, "Could not find the key " + key + " in PUSH_IDS"
+
+    return prefix + PUSH_IDS[key]
+
+
+class MockTreeherderServer(server.BaseHTTPRequestHandler):
+    def do_GET(self):
+
+        self.send_response(200)
+        self.send_header("Content-type", "application/json")
+        self.end_headers()
+
+        if EXPECTEDPATH_PUSH in self.path:
+            revision = self.path[self.path.index(EXPECTEDPATH_PUSH) + len(EXPECTEDPATH_PUSH):]
+            print("MockTreeherderServer: Looking for revision %s" % revision)
+
+            if revision not in TRY_REVISIONS:
+                assert False, "MockTreeherderServer: Could not find that revision"
+
+            self.wfile.write(TRY_REVISIONS[revision].encode())
+
+        elif EXPECTEDPATH_FAILURECLASSIFICATION in self.path:
+            self.wfile.write(FAILURE_CLASSIFICATIONS.encode())
+
+        elif EXPECTEDPATH_JOBS in self.path:
+            filename = get_appropriate_filename(self.path)
+            print("MockTreeherderServer: Streaming %s" % filename)
+
+            with open(filename, "rb") as f:
+                for line in f:
+                    self.wfile.write(line)
+        else:
+            assert False, "MockTreeherderServer got a path it didn't expect"
---
+++ tests/treeherder_api.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import sys
+import json
+import unittest
+
+from http import server
+from threading import Thread
+
+sys.path.append(".")
+sys.path.append("..")
+from components.logging import SimpleLoggerConfig
+from apis.taskcluster import TaskclusterProvider
+
+from tests.mock_commandprovider import TestCommandProvider
+from tests.mock_treeherder_server import MockTreeherderServer, FAILURE_CLASSIFICATIONS
+
+
+
+class TestTaskclusterProvider(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.server = server.HTTPServer(('', 27490), MockTreeherderServer)
+        cls.commandProvider = TestCommandProvider({})
+        cls.commandProvider.update_config(SimpleLoggerConfig)
+
+        cls.taskclusterProvider = TaskclusterProvider({
+            'url': 'http://localhost:27490/',
+        })
+        additional_config = SimpleLoggerConfig
+        additional_config.update({'CommandProvider': cls.commandProvider})
+        cls.taskclusterProvider.update_config(additional_config)
+
+        t = Thread(target=cls.server.serve_forever)
+        t.start()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.server.shutdown()
+        cls.server.server_close()
+
+    def testFailureClassification(self):
+        f = self.taskclusterProvider.failure_classifications
+        j = json.loads(FAILURE_CLASSIFICATIONS)
+        self.assertEqual(j[0]['name'], f[7])
+
+    def testPushExceptionHandling(self):
+        try:
+            self.taskclusterProvider.get_job_details('rev_broken')
+        except Exception:
+            self.assertTrue(True, "Did not see an exception that we expected.")
+        else:
+            self.assertTrue(False, "Expected an exception from taskclusterProvider but didn't see it.")
+
+    def testFunctionality(self):
+        job_list = self.taskclusterProvider.get_job_details('rev_good')
+        self.assertEqual(len(job_list), 3737, "Did not receive the correct number of jobs from the server.")
+
+    def testTransform(self):
+        properties = [
+            'hello',
+            'world'
+        ]
+        values = [
+            ["HI", "EARTH"],
+            ["WHAT", "UP"]
+        ]
+        data = self.taskclusterProvider._transform_job_list(properties, values)
+
+        self.assertEqual(data[0].hello, "HI")
+        self.assertEqual(data[0].world, "EARTH")
+
+        self.assertEqual(data[1].hello, "WHAT")
+        self.assertEqual(data[1].world, "UP")
+
+
+if __name__ == '__main__':
+    unittest.main()

6efb5dd16687921647d0037b134dc6c175b98c22
juniper wilde <juniper.r.wilde@gmail.com>
2020-09-14 08:20:39 -0700

    change repo url to updatebot

--- .circleci/pr_comment.sh
+++ .circleci/pr_comment.sh
@@ -3,7 +3,7 @@
 if [ $CIRCLE_BRANCH != "master" ]
 then
   # extract PR number from CircleCI environment variable and append to API url
-  curl --request POST "https://api.github.com/repos/metalcanine/pythonci/issues/${CIRCLE_PULL_REQUEST##*/}/comments" \
+  curl --request POST "https://api.github.com/repos/mozilla-services/updatebot/issues/${CIRCLE_PULL_REQUEST##*/}/comments" \
   -u $GH_USER:$GH_TOKEN \
   --header 'Accept: application/vnd.github.v3+json' \
   --data-raw "{\"body\": ${COMMAND_OUTPUT}}"

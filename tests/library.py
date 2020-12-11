fdf605c4e08e1098dea7fd4ad8dd05473d2b3674
juniper wilde <juniper.r.wilde@gmail.com>
2020-12-11 14:39:55 -0800

    Add validation step to moz.yaml import

--- tests/library.py
+++ tests/library.py
@@ -17,84 +17,38 @@ from components.logging import SimpleLoggerConfig
 
 LIBRARIES = [
     Struct(**{
-        "schema": 1,
         "bugzilla": {"product": "Core", "component": "Audio/Video: cubeb"},
         "origin": {
             "name": "cubeb",
-            "description": "Cross platform audio library",
-            "url": "https://github.com/kinetiknz/cubeb",
-            "license": "ISC",
-            "release": "a7e83aa2b1571b842a555158e8f25aeb1419ebd1 (2020-10-13 12:05:17 +0100)"
         },
         "updatebot": {
             "enabled": False,
-            "maintainer-needinfo": "nobody@mozilla.com",
+            "maintainer-bz": "nobody@mozilla.com",
             "maintainer-phab": "nobody"
         },
         "yaml_path": "{0}/.circleci/gecko-test/libcubeb/moz.yaml".format(os.getcwd())
     }),
     Struct(**{
-        "schema": 1,
         "bugzilla": {"product": "Core", "component": "Audio/Video: Playback"},
         "origin": {
             "name": "aom",
-            "description": "av1 decoder",
-            "url": "https://aomedia.googlesource.com/aom/",
-            "release": "commit 1e227d41f0616de9548a673a83a21ef990b62591 (Tue Sep 18 17:30:35 2018 +0000).",
-            "revision": "1e227d41f0616de9548a673a83a21ef990b62591",
-            "license": "BSD-2-Clause"
         },
-        "vendoring": {
-            "url": "https://aomedia.googlesource.com/aom",
-            "source-hosting": "googlesource",
-            "vendor-directory": "third_party/aom",
-            "exclude": ["build/.gitattributes", "build/.gitignore"],
-            "update-actions": [
-                {"action": "delete-path", "path": "{yaml_dir}/config"},
-                {
-                    "action": "run-script",
-                    "script": "{cwd}/generate_sources_mozbuild.sh",
-                    "cwd": "{yaml_dir}"
-                }
-            ]
+        "updatebot": {
+            "enabled": False,
+            "maintainer-bz": "",
+            "maintainer-phab": ""
         },
         "yaml_path": "{0}/.circleci/gecko-test/libaom/moz.yaml".format(os.getcwd())
     }),
     Struct(**{
-        "schema": 1,
         "bugzilla": {"product": "Core", "component": "Audio/Video: Playback"},
         "origin": {
             "name": "dav1d",
-            "description": "dav1d, a fast AV1 decoder",
-            "url": "https://code.videolan.org/videolan/dav1d",
-            "release": "commit 0243c3ffb644e61848b82f24f5e4a7324669d76e (2020-09-27T15:38:45.000+02:00).",
-            "revision": "0243c3ffb644e61848b82f24f5e4a7324669d76e",
-            "license": "BSD-2-Clause",
-            "license-file": "COPYING"
         },
         "updatebot": {
             "enabled": True,
-            "maintainer-phab": "nobody",
-            "maintainer-bz": "nobody@mozilla.com"
-        },
-        "vendoring": {
-            "url": "https://code.videolan.org/videolan/dav1d.git",
-            "source-hosting": "gitlab",
-            "vendor-directory": "third_party/dav1d",
-            "exclude": ["build/.gitattributes", "build/.gitignore"],
-            "update-actions": [
-                {
-                    "action": "copy-file",
-                    "from": "include/vcs_version.h.in",
-                    "to": "{yaml_dir}/vcs_version.h"
-                },
-                {
-                    "action": "replace-in-file",
-                    "pattern": "@VCS_TAG@",
-                    "with": "{revision}",
-                    "file": "{yaml_dir}/vcs_version.h"
-                }
-            ]
+            "maintainer-bz": "nobody@mozilla.com",
+            "maintainer-phab": "nobody"
         },
         "yaml_path": "{0}/.circleci/gecko-test/libdav1d/moz.yaml".format(os.getcwd())
     })
--- tests/mock_libraryprovider.py
+++ tests/mock_libraryprovider.py
@@ -18,40 +18,17 @@ class MockLibraryProvider(BaseProvider, INeedsCommandProvider, INeedsLoggingProv
 
     def get_libraries(self, gecko_path):
         return [Struct(**{
-            "schema": 1,
             "bugzilla": {"product": "Core", "component": "Audio/Video: Playback"},
             "origin": {
                 "name": "dav1d",
-                "description": "dav1d, a fast AV1 decoder",
-                "url": "https://code.videolan.org/videolan/dav1d",
-                "release": "commit 0243c3ffb644e61848b82f24f5e4a7324669d76e (2020-09-27T15:38:45.000+02:00).",
-                "revision": "0243c3ffb644e61848b82f24f5e4a7324669d76e",
-                "license": "BSD-2-Clause",
-                "license-file": "COPYING"
             },
             "updatebot": {
                 "enabled": True,
                 "maintainer-phab": "nobody",
                 "maintainer-bz": "nobody@mozilla.com"
             },
-            "vendoring": {
-                "url": "https://code.videolan.org/videolan/dav1d.git",
-                "source-hosting": "gitlab",
-                "vendor-directory": "third_party/dav1d",
-                "exclude": ["build/.gitattributes", "build/.gitignore"],
-                "update-actions": [
-                    {
-                        "action": "copy-file",
-                        "from": "include/vcs_version.h.in",
-                        "to": "{yaml_dir}/vcs_version.h"
-                    },
-                    {
-                        "action": "replace-in-file",
-                        "pattern": "@VCS_TAG@",
-                        "with": "{revision}",
-                        "file": "{yaml_dir}/vcs_version.h"
-                    }
-                ]
-            },
             "yaml_path": "mozilla-central/source/media/libdav1d/moz.yaml"
         })]
+
+    def validate_library(self, library):
+        pass

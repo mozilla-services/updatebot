c886da129ae80dedc245f21092eed22b4ce32d2d
Tom Ritter <tom@ritter.vg>
2020-12-22 17:21:32 -0500

    Convert Job Outcome and Status to proper enums which gives us access to the value <-> english name mapping.

--- components/dbmodels.py
+++ components/dbmodels.py
@@ -4,23 +4,37 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-from components.utilities import Struct
+from enum import unique, IntEnum
 
-JOBSTATUS = Struct(**{
-    'AWAITING_TRY_RESULTS': 1,
-    'AWAITING_RETRIGGER_RESULTS': 2,
-    'DONE': 3
-})
 
-JOBOUTCOME = Struct(**{
-    'PENDING': 1,
-    'COULD_NOT_VENDOR': 2,
-    'BUILD_FAILED': 3,
-    'CLASSIFIED_FAILURES': 4,
-    'UNCLASSIFIED_FAILURES': 5,
-    'ALL_SUCCESS': 6,
-    'ABORTED': 7
-})
+@unique
+class JOBSTATUS(IntEnum):
+    AWAITING_TRY_RESULTS = 1
+    AWAITING_RETRIGGER_RESULTS = 2
+    DONE = 3
+
+    # pymysql expects simple arguments, e.g. ints and strings.
+    # Rather than putting foo.value everywhere, we're just going
+    # to define the ultimate function pymysql calls to translate
+    # the value into unicode for the database operation, and
+    # hope this doesn't cause problems down the road.
+    def translate(self, _escape_table):
+        return str(self.value)
+
+
+@unique
+class JOBOUTCOME(IntEnum):
+    PENDING = 1
+    COULD_NOT_VENDOR = 2
+    BUILD_FAILED = 3
+    CLASSIFIED_FAILURES = 4
+    UNCLASSIFIED_FAILURES = 5
+    ALL_SUCCESS = 6
+    ABORTED = 7
+
+    # See above
+    def translate(self, _escape_table):
+        return str(self.value)
 
 
 class Job:
@@ -29,8 +43,8 @@ class Job:
             self.id = row['id']
             self.library_shortname = row['library']
             self.version = row['version']
-            self.status = row['status']
-            self.outcome = row['outcome']
+            self.status = JOBSTATUS(row['status'])
+            self.outcome = JOBOUTCOME(row['outcome'])
             self.bugzilla_id = row['bugzilla_id']
             self.phab_revision = row['phab_revision']
             self.try_revision = row['try_revision']

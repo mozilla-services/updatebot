cce655b541ab6f6029597d2e769a7b1dc62811ef
Tom Ritter <tom@ritter.vg>
2021-03-03 13:36:01 -0500

    If github errors on us, try again, reporting the github error

--- .circleci/pr_comment.sh
+++ .circleci/pr_comment.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-# restrict to running on non-main branches
-if [ $CIRCLE_BRANCH != "master" ]
-then
-  # extract PR number from CircleCI environment variable and append to API url
-  curl --request POST "https://api.github.com/repos/mozilla-services/updatebot/issues/${CIRCLE_PULL_REQUEST##*/}/comments" \
-  -u $GH_USER:$GH_TOKEN \
-  --header 'Accept: application/vnd.github.v3+json' \
-  --data-binary @json_output.txt
-fi
+
+# extract PR number from CircleCI environment variable and append to API url
+curl \
+--request POST "https://api.github.com/repos/mozilla-services/updatebot/issues/${CIRCLE_PULL_REQUEST##*/}/comments" \
+-u $GH_USER:$GH_TOKEN \
+--header 'Accept: application/vnd.github.v3+json' \
+--data-binary @json_output.txt \
+-o /dev/stderr \
+-w "%{http_code}"

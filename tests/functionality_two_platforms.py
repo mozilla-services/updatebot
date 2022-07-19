0f6e1c774edab1109020425384e7309dbbc4a6c0
Tom Ritter <tom@ritter.vg>
2022-07-19 16:40:15 -0400

    Cut over two-platform testSecondJobReferencesFirst

--- tests/functionality_two_platforms.py
+++ tests/functionality_two_platforms.py
@@ -863,6 +863,15 @@ class TestFunctionality(SimpleLoggingTest):
 
     # Create -> Finish -> Create
     def testSecondJobReferencesFirst(self):
+        @treeherder_response
+        def treeherder(request_type, fullpath):
+            if request_type == TYPE_HEALTH:
+                return "health_all_success.txt"
+            else:  # TYPE_JOBS
+                if treeherder.jobs_calls == 0:
+                    return "jobs_still_running.txt"
+                return "jobs_all_success.txt"
+
         call_counter = 0
 
         def git_pretty_output(since_last_job):
@@ -871,7 +880,7 @@ class TestFunctionality(SimpleLoggingTest):
                 "80240fe58a7558fc21d4f2499261a53f3a9f6fae|2020-11-12 10:01:18 +0000|2020-11-12 13:10:14 +0000",
                 "80240fe58a7558fc21d4f2499261a53f3a9f6faf|2020-11-12 07:00:44 +0000|2020-11-12 08:44:21 +0000",
             ]
-            if call_counter == 0:
+            if call_counter < 2:
                 assert not since_last_job
                 return lines[1:]
             else:
@@ -880,14 +889,18 @@ class TestFunctionality(SimpleLoggingTest):
                 return lines
 
         def get_filed_bug_id():
-            if call_counter == 0:
+            if call_counter < 2:
                 return 50
             return 51
 
         def get_filed_bugs(only_open):
             if call_counter == 0:
                 return []
-            return [50]
+            elif call_counter < 3:
+                return [50]
+            elif call_counter == 3:
+                return [50, 51]
+            self.assertTrue(False)
 
         global was_abandoned
         was_abandoned = False
@@ -899,12 +912,13 @@ class TestFunctionality(SimpleLoggingTest):
             return CONDUIT_EDIT_OUTPUT
 
         library_filter = 'dav1d'
-        (u, expected_values, _check_jobs) = TestFunctionality._setup(
+        (u, expected_values, _check_jobs) = self._setup(
             library_filter,
             git_pretty_output,
             lambda: ["80240fe58a7558fc21d4f2499261a53f3a9f6fad", "56AAAAAAacfacba40993e47ef8302993c59e264e"],
             get_filed_bug_id,
             get_filed_bugs,
+            treeherder,
             command_callbacks={'abandon': abandon_callback}
         )
 
@@ -913,36 +927,47 @@ class TestFunctionality(SimpleLoggingTest):
             u.run(library_filter=library_filter)
             # Check that we created the job successfully
             _check_jobs(JOBSTATUS.AWAITING_INITIAL_PLATFORM_TRY_RESULTS, JOBOUTCOME.PENDING)
+
             # Run it again, this time we'll tell it the jobs are still in process
             u.run(library_filter=library_filter)
             _check_jobs(JOBSTATUS.AWAITING_INITIAL_PLATFORM_TRY_RESULTS, JOBOUTCOME.PENDING)
+
+            call_counter += 1  # See (**)
+
             # Run it again, this time we'll tell it the jobs are done
             u.run(library_filter=library_filter)
             _check_jobs(JOBSTATUS.AWAITING_SECOND_PLATFORMS_TRY_RESULTS, JOBOUTCOME.PENDING)
+
             # Run it again, this time we'll tell it everything succeeded
             u.run(library_filter=library_filter)
             _check_jobs(JOBSTATUS.DONE, JOBOUTCOME.ALL_SUCCESS)
             self.assertFalse(was_abandoned, "Prematurely abandoned the phabricator patch.")
 
             call_counter += 1
-            reset_seen_counters()
 
             # Run it
             u.run(library_filter=library_filter)
             # Check that we created the job successfully
             _check_jobs(JOBSTATUS.AWAITING_INITIAL_PLATFORM_TRY_RESULTS, JOBOUTCOME.PENDING)
-            # Run it again, this time we'll tell it the jobs are still in process
-            u.run(library_filter=library_filter)
-            _check_jobs(JOBSTATUS.AWAITING_INITIAL_PLATFORM_TRY_RESULTS, JOBOUTCOME.PENDING)
-            # Run it again, this time we'll tell it the jobs are done
+
+            call_counter += 1
+
+            # Run it again, go to the next platform
             u.run(library_filter=library_filter)
             _check_jobs(JOBSTATUS.AWAITING_SECOND_PLATFORMS_TRY_RESULTS, JOBOUTCOME.PENDING)
-            # Run it again, this time we'll tell it everything succeeded
+
+            # Run it
             u.run(library_filter=library_filter)
+            # Should be DONE and Success
             _check_jobs(JOBSTATUS.DONE, JOBOUTCOME.ALL_SUCCESS)
-            self.assertTrue(was_abandoned, "Did not successfully abandon the phabricator patch as expected.")
+
+            all_jobs = u.dbProvider.get_all_jobs()
+            self.assertEqual(len([j for j in all_jobs if library_filter in j.library_shortname]), 2, "I should have two jobs.")
+            self.assertEqual(all_jobs[1].outcome, JOBOUTCOME.ALL_SUCCESS, "The first job should still be successful.")
+            self.assertTrue(all_jobs[1].relinquished, "The first job should be relinquished.")
+            self.assertTrue(was_abandoned, "Did not successfully abandon the phabricator patch.")
         finally:
-            TestFunctionality._cleanup(u, expected_values)
+            self._cleanup(u, expected_values)
 
     # Create -> (Not Done) -> Create
     def testSecondJobButFirstIsntDone(self):

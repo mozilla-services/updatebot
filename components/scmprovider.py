dec012e516f06b06774fdde16d605b36f0dcaf1f
Erich Gubler <erichdongubler@gmail.com>
2025-11-17 15:43:02 -0500

    fix(scm): correctly check hostnames for repo URL generation

--- components/scmprovider.py
+++ components/scmprovider.py
@@ -14,7 +14,13 @@ from components.providerbase import BaseProvider, INeedsCommandProvider, INeedsL
 
 
 def repo_and_commit_to_url(repo, commit):
-    if "https://chromium.googlesource.com" or 'https://https://aomedia.googlesource.com' in repo:
+    if any(
+        hostname in repo
+        for hostname in [
+            "https://chromium.googlesource.com",
+            "https://https://aomedia.googlesource.com",
+        ]
+    ):
         return repo + "/+/" + commit
 
     # gitlab wants https://domain.com/org/project/-/commit/sha
